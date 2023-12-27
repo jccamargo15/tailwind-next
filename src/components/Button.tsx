b4af4ce7992a5012319c4e81ea1b990a43015765
José Carlos de Camargo <jccamargo15@gmail.com>
2023-12-27 16:35:21 -0300

    feat: Variantes Ghost dos Botões

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,6 +12,7 @@ const button = tv({
     variant: {
       primary: 'bg-violet-600 text-white hover:bg-violet-700',
       outline: 'text-zinc-700 border border-zinc-300 hover:bg-zinc-50',
+      ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
     },
   },
 
